ca4d33a30530c3cf1f4a227e00fb48c84a4a358d
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2022-01-13 17:46:20 +0100

    a

--- MyFirst3DProject/Assets/Scripts/Pelota.cs
+++ MyFirst3DProject/Assets/Scripts/Pelota.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Pelota : MonoBehaviour
 {
-// Este script es otra forma de mover la bola siguiendo el vídeo del curso cefire
+    // Este script es otra forma de mover la bola siguiendo el vídeo del curso cefire
     float velocidadAvance = 2.0f; // 2 m/s
     float velocidadRotac = 180.0f; // 180 grados por segundo
-// Start is called before the first frame update
+    // Start is called before the first frame update
     void Start()
     {
+        
     }
 // Update is called once per frame
     void Update()
     {
-// Para coger el avance con las flechas adelante y atrás
+        // Para coger el avance con las flechas adelante y atrás
         float avance = Input.GetAxis("Vertical")
                        * velocidadAvance * Time.deltaTime;
-// La rotación con las flechas izq y dcha.
+        // La rotación con las flechas izq y dcha.
         float rotacion = Input.GetAxis("Horizontal")
                          * velocidadRotac * Time.deltaTime;
         transform.Translate(Vector3.forward * avance);
