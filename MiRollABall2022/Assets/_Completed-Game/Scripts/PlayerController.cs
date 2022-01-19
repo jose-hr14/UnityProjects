a4b473dca0d01407367e96f0c37a1b62fa9a2cf2
jose-hr14 <71669454+jose-hr14@users.noreply.github.com>
2022-01-19 19:00:50 +0100

    a

--- MiRollABall2022/Assets/_Completed-Game/Scripts/PlayerController.cs
+++ MiRollABall2022/Assets/_Completed-Game/Scripts/PlayerController.cs
@@ -1,13 +1,9 @@
 ﻿using UnityEngine;
-
-// Include the namespace required to use Unity UI
 using UnityEngine.UI;
-
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
-	
-	// Create public variables for player speed, and for the Text UI game objects
+public class PlayerController : MonoBehaviour {	
+	// Creamos variables publicas para al velocidad del jugador y para los textos del canvas
 	public float speed;
 	public Text countText;
 	public Text winText;
@@ -16,74 +12,84 @@ public class PlayerController : MonoBehaviour {
 	public Transform particulasPared;
 	public Transform particulasPickUp;
 
-	// Create private references to the rigidbody component on the player, and the count of pick up objects picked up so far
+	// Creamos referencias privadas al componente rigidbody del jugador, y para al cuenta de pick ups que se vayan recogiendo
 	private Rigidbody rb;
 	private int count;
 
-	// At the start of the game..
+	// Al comienzo del juego...
 	void Start ()
 	{
-		// Assign the Rigidbody component to our private rb variable
+		// Asignamos a la variable rb la dirección de memoria del rigidbody, y a la variable r la del renderes
 		rb = GetComponent<Rigidbody>();
 		r = GetComponent<Renderer>();
 
-		// Set the count to zero 
+		// Ponemos la cuenta de pick ups a cero
 		count = 0;
 
-		// Run the SetCountText function to update the UI (see below)
+		// Llamamos a al función SetCountText para actualizar la interfaz de usuario
 		SetCountText ();
 
-		// Set the text property of our Win Text UI to an empty string, making the 'You Win' (game over message) blank
+		// Pone el texto de victoria como vacío
 		winText.text = "";
 	}
-
+	//Se llamará por cada frame
     private void Update()
     {
+		// Al pulsar el botón de saltar, y si la pelota está sobre el suelo, ejerce una fuerza
+		// en el eje y hacia arriba que impulsa al pelota haciéndola saltar
         if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.1F)
         {
 			rb.AddForce(Vector3.up * fuerzaDeSalto, ForceMode.Impulse);
         }
     }
-
-    // Each physics step..
+    // Se llamará cada 0.2 segundo o cual sea el lapso de tiempo que se haya elegido en la configuracion de unity
+	// Aquí irá el código relativo a las físicas del juego, pues queremos que se ejecturene en un lapso de tiempo
+	// constante
     void FixedUpdate ()
 	{
-		// Set some local float variables equal to the value of our Horizontal and Vertical Inputs
+		// Declaramos algunas variables locales equivalentes al input horizontal y vertical
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
 		// Create a Vector3 variable, and assign X and Z to feature our horizontal and vertical float variables above
+		// Creamos una variable Vector3, le asignamos a la X el valor que saquemos del input horizonta, y z al que 
+		// saquemos del input vertical
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 
-		// Add a physical force to our Player rigidbody using our 'movement' Vector3 above, 
-		// multiplying it by 'speed' - our public player speed that appears in the inspector
+		// Añadimos una fuerza física al rigidbody del jugar utilizando el vector que hemos creado
+		// con los valores del input horizontal y vertical leídos del control. Esto lo multiplicamos
+		// por el atributo público de clase de la velocidad
 		rb.AddForce (movement * speed);
 	}
 
-	// When this game object intersects a collider with 'is trigger' checked, 
-	// store a reference to that collider in a variable named 'other'..
+	// Cuando el juego entre en contacto con un collider que tenga marcado el is trigger
+	// guardará en la variable other una referencia a ese collider...
 	void OnTriggerEnter(Collider other) 
 	{
-		// ..and if the game object we intersect has the tag 'Pick Up' assigned to it..
+		// y si el objeto is trigger tiene la etiqueta de Pick Up asignada...
 		if (other.gameObject.CompareTag ("Pick Up"))
 		{
-			// Make the other game object (the pick up) inactive, to make it disappear
+			// Pone el objeto como inactivo, haciéndolo desaparecer
 			other.gameObject.SetActive (false);
 
-			// Add one to the score variable 'count'
+			// Suma 1 al contador de pick ups
 			count = count + 1;
 
-			// Run the 'SetCountText()' function (see below)
+			// Llama al procedimiento SetCountText()
 			SetCountText ();
 
-			if (transform.localScale.x < 3)
+			// Si la escala en el eje x es menor que cinco, incrementamos su tamaño en 0.05 en todos su ejes
+			if (transform.localScale.x < 5)
 				transform.localScale += new Vector3(0.05f, 0.05f, 0.05f);
+			// Instanciamos un sistema de partículas en la posición en la que estaba la bola en el momento de
+			// coger el pick up, y lo destruye al segundo después
 			Transform particulasPickUpInstanciadas = Instantiate(particulasPickUp, gameObject.transform.position, Quaternion.identity);			
 			Destroy(particulasPickUpInstanciadas.gameObject, 1);
 		}
 	}
 
-	// Create a standalone function that can update the 'countText' UI and check if the required amount to win has been achieved
+	// Procedimiento que actualiza el texto del conteo de pick ups y comprueba se ha han cogido todos para mostrar el texto de 
+	// victoria
 	void SetCountText()
 	{
 		// Update the text field of our 'countText' variable
@@ -119,7 +125,7 @@ public class PlayerController : MonoBehaviour {
 		{
 			r.material.color = new Color(Mathf.Round(Random.value),
 				Mathf.Round(Random.value), Mathf.Round(Random.value));
-			if (transform.localScale.x > 0.5)
+			if (transform.localScale.x > 1)
 				transform.localScale -= new Vector3(0.5f, 0.5f, 0.5f);
 			if (collision.transform.localScale.y > 3)
 			{
--- MiRollABall2022/Assets/coche.cs
+++ MiRollABall2022/Assets/coche.cs
@@ -4,29 +4,85 @@ using UnityEngine;
 
 public class coche : MonoBehaviour
 {
-    // Este script es otra forma de mover la bola siguiendo el vídeo del curso cefire
-    float velocidadAvance = 2.0f; // 2 m/s
-    float velocidadRotac = 180.0f; // 180 grados por segundo
-    [SerializeField] float xInicial;
-    [SerializeField] float zInicial;
-    [SerializeField] int vidas;
-    // Start is called before the first frame update
+    public WheelCollider WheelFL;
+    public WheelCollider WheelFR;
+    public WheelCollider WheelRL;
+    public WheelCollider WheelRR;
+    public Transform WheelFLtrans;
+    public Transform WheelFRtrans;
+    public Transform WheelRLtrans;
+    public Transform WheelRRtrans;
+    public Vector3 eulertest;
+    float maxFwdSpeed = -3000;
+    float maxBwdSpeed = 1000f;
+    float gravity = 9.8f;
+    private bool braked = false;
+    private float maxBrakeTorque = 500;
+    private Rigidbody rb;
+    public Transform centreofmass;
+    private float maxTorque = 1000;
     void Start()
     {
-        xInicial = transform.position.x;
-        zInicial = transform.position.z;
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = centreofmass.transform.localPosition;
     }
-    // Update is called once per frame
+
+    void FixedUpdate()
+    {
+        if (!braked)
+        {
+            WheelFL.brakeTorque = 0;
+            WheelFR.brakeTorque = 0;
+            WheelRL.brakeTorque = 0;
+            WheelRR.brakeTorque = 0;
+        }
+        //speed of car, Car will move as you will provide the input to it.
+
+        WheelRR.motorTorque = maxTorque * Input.GetAxis("Vertical");
+        WheelRL.motorTorque = maxTorque * Input.GetAxis("Vertical");
+
+        //changing car direction
+        //Here we are changing the steer angle of the front tyres of the car so that we can change the car direction.
+        WheelFL.steerAngle = 30 * (Input.GetAxis("Horizontal"));
+        WheelFR.steerAngle = 30 * Input.GetAxis("Horizontal");
+    }
     void Update()
     {
-        // Para coger el avance con las flechas adelante y atrás
-        float avance = Input.GetAxis("Vertical")
-                       * velocidadAvance * Time.deltaTime;
-        // La rotación con las flechas izq y dcha.
-        float rotacion = Input.GetAxis("Horizontal")
-                         * velocidadRotac * Time.deltaTime;
-        transform.Translate(Vector3.forward * avance);
-        transform.Rotate(Vector3.up * rotacion);
+        HandBrake();
+
+        //for tyre rotate
+        WheelFLtrans.Rotate(WheelFL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        WheelFRtrans.Rotate(WheelFR.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        WheelRLtrans.Rotate(WheelRL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        WheelRRtrans.Rotate(WheelRL.rpm / 60 * 360 * Time.deltaTime, 0, 0);
+        //changing tyre direction
+        Vector3 temp = WheelFLtrans.localEulerAngles;
+        Vector3 temp1 = WheelFRtrans.localEulerAngles;
+        temp.y = WheelFL.steerAngle - (WheelFLtrans.localEulerAngles.z);
+        WheelFLtrans.localEulerAngles = temp;
+        temp1.y = WheelFR.steerAngle - WheelFRtrans.localEulerAngles.z;
+        WheelFRtrans.localEulerAngles = temp1;
+        eulertest = WheelFLtrans.localEulerAngles;
+    }
+    void HandBrake()
+    {
+        //Debug.Log("brakes " + braked);
+        if (Input.GetButton("Jump"))
+        {
+            braked = true;
+        }
+        else
+        {
+            braked = false;
+        }
+        if (braked)
+        {
+
+            WheelRL.brakeTorque = maxBrakeTorque * 20;//0000;
+            WheelRR.brakeTorque = maxBrakeTorque * 20;//0000;
+            WheelRL.motorTorque = 0;
+            WheelRR.motorTorque = 0;
+        }
     }
 
 }
